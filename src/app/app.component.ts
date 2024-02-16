79e20b1b58594c72a9cf20e3c4b4d3d7ff657728
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-02-16 15:32:45 +0200

    add initial task solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,73 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+type Strength = 'easy' | 'medium' | 'strong';
+enum SectionColors {
+  grey = '#a3a2a2',
+  red = '#bb2f2f',
+  green = '#33a033',
+  yellow = '#d8ae3a',
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'test-password-strength-app';
+  password: string = '';
+  sections: SectionColors[] = [SectionColors.grey, SectionColors.grey, SectionColors.grey];
+  minPasswordLength = 8;
+
+  getPasswordStrength(): Strength {
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasDigits = /\d/.test(this.password);
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+
+    if ((!hasLetters && !hasDigits)
+      || (!hasDigits && !hasSymbols)
+      || (!hasLetters && !hasSymbols)) {
+      return 'easy';
+    }
+
+    if (!hasLetters || !hasDigits || !hasSymbols) {
+      return 'medium';
+    }
+
+    return 'strong';
+  }
+
+  updateSectionColors(): void {
+    if (!this.password) {
+      this.sections = this.sections.map(() => SectionColors.grey);
+      return;
+    }
+
+    if (this.password.length < this.minPasswordLength) {
+      this.sections = this.sections.map(() => SectionColors.red);
+      return;
+    }
+
+    const strength = this.getPasswordStrength();
+
+    switch (strength) {
+      case 'easy':
+        this.sections = [SectionColors.red, SectionColors.grey, SectionColors.grey];
+        break;
+
+      case 'medium':
+        this.sections = [SectionColors.yellow, SectionColors.yellow, SectionColors.grey];
+        break;
+
+      case 'strong':
+        this.sections = this.sections.map(() => SectionColors.green);
+    }
+  }
 }
